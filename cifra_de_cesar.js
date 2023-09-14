f914e3ff74f144ed9caaf5fe58de54999e2ce155
ThiCamillo <thiago.sam.camillo@gmail.com>
2023-09-13 21:41:36 -0300

    cifra de cesar

---
+++ cifra_de_cesar.js
@@ -0,0 +1,14 @@
+function criptografa(mensagem, chave) {
+
+    for (let i = 0; i < mensagem.length; i++) {
+
+    }
+}
+
+const CHAVE = 3;
+const mensagem = 'CODIGO DE CESAR';
+const conteudoCifrado
+
+
+
+const mensagem = 'conteudo secreto';
